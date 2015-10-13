75b40428c2fa633358344974e3ae4bfc6af1eef2
thiphamyp <thi.pham@store-factory.com>
2015-10-13 10:54:07 +0200

    Add Api Customer

---
+++ lib/Api/Customer.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace Zoho\Subscription\Api;
+
+use Zoho\Subscription\Client\Client;
+
+/**
+ * @author Hang Pham <thi@yproximite.com>
+ * @author Tristan Bessoussa <tristan.bessoussa@gmail.com>
+ *
+ * @link https://www.zoho.com/subscriptions/api/v1/#customers
+ */
+class Customer extends Client
+{
+    /**
+     * @param string $customerEmail The customer's email
+     *
+     * @throws \Exception
+     *
+     * @return array
+     */
+    public function getCustomerByEmail($customerEmail)
+    {
+        $cacheKey = sprintf('zoho_customer_%s', $customerEmail);
+        $hit      = $this->getFromCache($cacheKey);
+
+        if (false === $hit) {
+            $response = $this->client->request('GET', 'customers', [
+                'query' => ['email' => $customerEmail]
+            ]);
+
+            $result = $this->processResponse($response);
+
+            $customer = $result['customer'];
+
+            $this->saveToCache($cacheKey, $customer);
+
+            return $customer;
+        }
+
+        return $hit;
+    }
+
+    /**
+     * @param string $customerId The customer's id
+     *
+     * @throws \Exception
+     *
+     * @return array
+     */
+    public function getCustomerById($customerId)
+    {
+        $cacheKey = sprintf('zoho_customer_%s', $customerId);
+        $hit      = $this->getFromCache($cacheKey);
+
+        if (false === $hit) {
+            $response = $this->client->request('GET', sprintf('customers/%s', $customerId));
+            $result = $this->processResponse($response);
+
+            $customer = $result['customer'];
+
+            $this->saveToCache($cacheKey, $customer);
+
+            return $customer;
+        }
+
+        return $hit;
+    }
+}
