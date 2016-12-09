671c7e2b07c2b9c9ee5f1d34332f26b4f7b05845
brunoroux <bruno@yproximite.com>
2016-12-09 11:45:10 +0100

    added hosted page client

---
+++ lib/Api/HostedPage.php
@@ -0,0 +1,44 @@
+<?php
+declare(strict_types=1);
+
+namespace Zoho\Subscription\Api;
+
+use Zoho\Subscription\Client\Client;
+
+class HostedPage extends Client
+{
+    public function listHostedPages(): array
+    {
+        $response = $this->sendRequest('GET', 'hostedpages', ['content-type' => 'application/json'], json_encode($data));
+
+        return $this->processResponse($response);
+    }
+
+    public function createSubscription(array $data): array
+    {
+        $response = $this->sendRequest('POST', 'hostedpages/newsubscription', ['content-type' => 'application/json'], json_encode($data));
+
+        return $this->processResponse($response);
+    }
+
+    public function updateSubscription(array $data): array
+    {
+        $response = $this->sendRequest('POST', 'hostedpages/updatesubscription', ['content-type' => 'application/json'], json_encode($data));
+
+        return $this->processResponse($response);
+    }
+
+    public function updateCard(array $data): array
+    {
+        $response = $this->sendRequest('POST', 'hostedpages/updatecard', ['content-type' => 'application/json'], json_encode($data));
+
+        return $this->processResponse($response);
+    }
+
+    public function buyOneTimeAddon(array $data): array
+    {
+        $response = $this->sendRequest('POST', 'hostedpages/buyonetimeaddon', ['content-type' => 'application/json'], json_encode($data));
+
+        return $this->processResponse($response);
+    }
+}
